89bb44b1747f570dc0374b184350359f6a05ae16
Nicole Marty <nmarty310@gmail.com>
2019-09-28 13:39:51 -0500

    duh. both questions can go under one prompt.

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -31,47 +31,44 @@ function afterConnection() {
 
 function start() {
     inquirer
-        .prompt({
-            name: "ID_selection",
-            type: "list",
-            message: "What is the ID of the product you would like to buy?",
-            choices: ["1001", "1002", "1003", "1004", "1005",
-                "1006", "1007", "1008", "1009", "1010"
-            ]
-        })
+        .prompt([{
+                name: "ID_selection",
+                type: "list",
+                message: "What is the ID of the product you would like to buy?",
+                choices: ["1001", "1002", "1003", "1004", "1005",
+                    "1006", "1007", "1008", "1009", "1010"
+                ]
+            },
+            {
+                name: "units",
+                type: "number",
+                message: "How many units of the product would you like to buy?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
         .then(function(answer) {
             if (answer.ID_selection === "1001" || "1002" || "1003" || "1004" || "1005" ||
                 "1006" || "1007" || "1008" || "1009" || "1010")
                 console.log("Thank you");
-            messageTwo();
-        })
-}
-
-function messageTwo() {
-    inquirer
-        .prompt({
-            name: "units",
-            type: "number",
-            message: "How many units of the product would you like to buy?",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        })
-        .then(function(answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query(
-                "UPDATE products SET ?", {
-                    stock_quantity: 2
-                },
-                function(err) {
-                    if (err) throw err;
-                    console.log("Your order was created successfully!");
-                    // re-prompt the user for if they want to bid or post
-                    afterConnection();
-                }
-            );
+            stockCheck();
         });
-}
+};
+
+
+function stockCheck() {
+    connection.query(
+        "UPDATE products SET ?", {
+            stock_quantity: 2
+        },
+        function(err) {
+            if (err) throw err;
+            console.log("Your order was created successfully!");
+            afterConnection();
+        }
+    );
+};
